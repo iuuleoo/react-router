e035f881c6cda86be30bbc5decfd6285ed788b0a
Leo <leooproencaaa@gmail.com>
2025-12-02 18:55:47 -0300

    Retornando para Página Anterior

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,12 +1,15 @@
-import { useParams } from "react-router"
+import { useParams, useNavigate } from "react-router"
 
 
 export function Details() {
 
     const { id } = useParams()
+    const navigate = useNavigate()
+
 
     return (
         <div>
+            <button type="button" onClick={() => navigate("/products")}>Voltar</button>
             <h1>Detalhes</h1>
 
             <span>
