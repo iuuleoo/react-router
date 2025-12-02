239c9cca4691f3e10c05f7667df4c0ed81e135da
Leo <leooproencaaa@gmail.com>
2025-12-02 18:04:41 -0300

    Criando a Primeira Rota

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router"
 
 import './main.css'
 
-import App from './App.tsx'
+import { AppRoutes } from './routes/AppRoutes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <AppRoutes />
+    </BrowserRouter>
   </StrictMode>,
 )
