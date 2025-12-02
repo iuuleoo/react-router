2120528dc952ca2b25b513af39ad09662bb23453
Leo <leooproencaaa@gmail.com>
2025-12-02 18:20:42 -0300

    Página 404

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,11 +2,16 @@ import { Routes, Route } from "react-router"
 
 import { Home } from "../pages/Home"
 import { Products } from "../pages/Product"
+import { NotFound } from "../pages/NotFound"
+
+
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" index element={<Home />} />
             <Route path="/products" index element={<Products />} />
+
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
