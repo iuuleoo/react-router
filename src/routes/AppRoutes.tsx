71b8c73806f53e59ef59ed5a0f18f4673ecf2206
Leo <leooproencaaa@gmail.com>
2025-12-02 18:09:47 -0300

    Rota de Produtos

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router"
 
 import { Home } from "../pages/Home"
-
+import { Products } from "../pages/Product"
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" index element={<Home />} />
+            <Route path="/products" index element={<Products />} />
         </Routes>
     )
 }
