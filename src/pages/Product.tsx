3003757538153b6b4b831dec8ea0abea97b1ca70
Leo <leooproencaaa@gmail.com>
2025-12-02 18:31:41 -0300

    useSearchParams

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,7 +1,26 @@
+import { useSearchParams } from "react-router"
+
+
 export function Products() {
+    const [searchParams] = useSearchParams()
+
+    const category = searchParams.get("category")
+    const price = searchParams.get("price")
+
+
     return (
         <div>
             <a href="/">Voltar</a>
+
+            <h1>Produtos</h1>
+
+            {
+                category && (
+                    <span>
+                        Categoria <strong>{category}</strong>
+                    </span>
+                )
+            }
         </div>
     )
 }
